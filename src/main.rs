ad4077a933631f8a2a4bee9aa7484550528dc3dc
Nazmul Alom <nazmulshuvo03@gmail.com>
2024-03-01 21:56:00 +0600

    stage 5: implement echo command, done

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,76 @@ use std::{
     thread,
 };
 
+fn handle_response(mut stream: TcpStream) {
+    let mut buff = [0; 512];
+    let separator = "\r\n";
+
+    loop {
+        let bytes_read = stream.read(&mut buff).expect("Failed to read stream");
+        println!("bytes read: {:?}", bytes_read);
+
+        if bytes_read == 0 {
+            eprintln!("0 Bytes read from stream");
+            return;
+        }
+        let buff_vec = buff[..bytes_read].to_vec();
+        println!("Buffer vector: {:?}", buff_vec);
+        let raw_input = String::from_utf8(buff_vec).expect("String read failed");
+        let raw_input_str = raw_input.as_str();
+        println!("Raw input string: {:?}", raw_input_str);
+        raw_input_str.chars().for_each(|c| print!("{} ", c));
+        let raw_input_vec: Vec<&str> = raw_input_str.split(separator).collect();
+        println!("Raw input vector: {:?}", raw_input_vec);
+
+        // let mut first: u32 = 0;
+        // match raw_input_vec.first() {
+        //     Some(v) => {
+        //         println!("first: {:?}", v);
+        //         match v.strip_prefix("*") {
+        //             Some(num) => {
+        //                 println!("Number from first: {:?}", num);
+        //                 let value: u32 = num.parse::<u32>().unwrap();
+        //                 first = value;
+        //             }
+        //             None => println!("Could not found * in first element"),
+        //         }
+        //     }
+        //     None => println!("There is no element in raw input array"),
+        // };
+        // println!("Value from first element: {}", first);
+
+        let command = raw_input_vec[2];
+
+        match command {
+            "ping" => {
+                let res = format!("{}{}", "+PONG", separator);
+                println!("ping command response: {:?}", res);
+                stream
+                    .write_all(res.as_bytes())
+                    .expect("Failed to write respnse");
+            }
+            "echo" => {
+                let res = format!(
+                    "{}{}{}{}",
+                    raw_input_vec[3], separator, raw_input_vec[4], separator
+                );
+                println!("echo command respnse: {:?}", res);
+                stream
+                    .write_all(res.as_bytes())
+                    .expect("Failed to write respnse");
+            }
+            _ => {
+                println!("Undefined command");
+            }
+        }
+    }
+}
+
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
     // Uncomment this block to pass the first stage
-    // TcpListener == "server"; TcpListener::bind -> create server
-    // TcpStream == "client"; TcpStream::connect -> connect to a server
     let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
 
     for stream in listener.incoming() {
@@ -25,22 +88,6 @@ fn main() {
             Err(e) => {
                 println!("error: {}", e);
             }
-        }
-    }
-}
-
-fn handle_response(mut stream: TcpStream) {
-    let mut buff = [0; 512];
-
-    loop {
-        let bytes_read = stream.read(&mut buff).expect("Failed to read stream");
-
-        if bytes_read == 0 {
-            return;
-        }
-        match stream.write_all(b"+PONG\r\n") {
-            Ok(_) => println!("Write successful"),
-            Err(e) => println!("Error writing stream: {}", e),
         }
     }
 }
